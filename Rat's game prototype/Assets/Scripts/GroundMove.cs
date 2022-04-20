3b2fe7756796aca392772cc665d6b7a210ee1724
Wasd-Qwerty <kylikov_yt@mail.ru>
2022-04-20 23:03:23 +0300

    v0.1
    Теперь на GitHub

---
+++ Rat's game prototype/Assets/Scripts/GroundMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMove : MonoBehaviour
+{
+    public float speed;
+    public float lowerXValue = -8;
+    private float destroyXvalue = -30;
+    private float upperXValue = 17;
+    public GameObject pipe;
+    private bool Instant = false;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        pipe.transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, 0,0);
+        if (pipe.transform.position.x <= lowerXValue && !Instant)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(upperXValue, 0, 0);
+            Instant = true;
+        }
+        if (pipe.transform.position.x <= lowerXValue && !Instant)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(upperXValue, 0, 0);
+            Instant = true;
+        }
+        if (pipe.transform.position.x < destroyXvalue)
+        {
+            Destroy(pipe);
+        }
+    }
+}
---
+++ Rat's game prototype/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public GameManager gameManager;
+    private Rigidbody2D rb;
+
+    public bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = Vector2.up * 40f;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Коллизия с " + collision.gameObject.tag);
+        if (collision.gameObject.tag == "ObjectResp")
+            gameManager.GameOver();
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
+    }
+}
