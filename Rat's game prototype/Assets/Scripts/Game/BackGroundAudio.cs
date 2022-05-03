bc6b2c53c280812107be92c7ca3f343baecc497c
Wasd-Qwerty <kylikov_yt@mail.ru>
2022-05-03 03:06:27 +0300

    ура обновление
    звук

---
+++ Rat's game prototype/Assets/Scripts/Game/BackGroundAudio.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundAudio : MonoBehaviour
+{
+    public string createdTag;
+    private void Awake()
+    {
+        GameObject obj = GameObject.FindWithTag(this.createdTag);
+        if (obj != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            this.gameObject.tag = this.createdTag;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
--- Rat's game prototype/Assets/Scripts/Game/MenuManager.cs
+++ Rat's game prototype/Assets/Scripts/Game/MenuManager.cs
@@ -6,14 +6,37 @@ using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
-    public Sprite newSpritePlay, newSpriteQuit;
-    public Image play, quit;
+    public Sprite newSpritePlay, newSpriteQuit, AudioOn, AudioOff;
+    private GameObject AudioSource;
+    public string audioTag;
+    public Image play, quit, AudioController;
     private void Start() {
         Time.timeScale = 1;
+        AudioSource = GameObject.FindWithTag(audioTag);
+        if (AudioSource.GetComponent<AudioSource>().enabled)
+        {
+            AudioController.sprite = AudioOn;
+        }
+        else
+        {
+            AudioController.sprite = AudioOff;
+        }
     }
     public void Play(){
         play.sprite = newSpritePlay;
         SceneManager.LoadScene(1);
+    }
+    public void AudioControl()
+    {
+        if (AudioSource.GetComponent<AudioSource>().enabled)
+        {
+            AudioController.sprite = AudioOff;
+        }
+        else
+        {
+            AudioController.sprite = AudioOn;
+        }
+        AudioSource.GetComponent<AudioSource>().enabled = !AudioSource.GetComponent<AudioSource>().enabled;
     }
     public void Quit(){
         quit.sprite = newSpriteQuit;
